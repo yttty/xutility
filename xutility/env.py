b982aaf4048b72835c8386e8b7c2f75f81333249
yttty <yttty@users.noreply.github.com>
2025-11-10 18:34:50 +0800

    feat: add default option in get_env

--- xutility/env.py
+++ xutility/env.py
@@ -1,9 +1,9 @@
 import os
 
 
-def get_env(keys: list[str]) -> str | None:
+def get_env(keys: list[str], default: str | None = None) -> str | None:
     """Get a environment variable by a list of possible keys"""
     for k in keys:
         if v := os.getenv(k):
             return v
-    return None
+    return default
